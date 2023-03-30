395a16864238400e6a7f7012bd4203bce5ad58d6
Sam Zudell <s.zudell98@gmail.com>
2023-03-30 09:16:49 -0400

    changed rendering of pokeList to avoid rendering before data was fetched. styled for small screen sizes

--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -4,31 +4,42 @@ export default function HomePage(props) {
 
   const [pokeList, setPokeList] = useState({})
 
+
   function getPokemon(){
+   
     fetch("https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json")
     .then((res) => res.json())
     .then((pokemon) => {
-      return setPokeList(pokemon)})
+      setPokeList(pokemon)})
     .catch((err) => console.error(err))
   }
+
 
   useEffect(() => {
     getPokemon()
   }, [])
 
+
+  
+
   return (
     <main>
       <div className='poke-list'>
-      {Object.entries(pokeList)[0][1].map((poke) => {
+        {console.log(pokeList.pokemon)}
+        { pokeList.pokemon ? pokeList.pokemon.map((poke) => {
         return (
         <div className='poke' key={poke.id}>
           <img src={poke.img} alt={poke.name} />
-          <p>{poke.num}</p>
-          <h3>{poke.name}</h3>
-          <p>{poke.type[0]}</p>
-          <p>{poke.type[1]}</p>
-        </div>)
-      })}
+          <div className="poke-info">
+            <p>{poke.num}</p>
+            <h3>{poke.name}</h3>
+            <div className='poke-type'>
+              <p>{poke.type[0]}</p>
+              <p>{poke.type[1]}</p>
+            </div>
+          </div>
+        </div>) 
+      }) : <>loading...</>}
       </div>
     </main>
   )
